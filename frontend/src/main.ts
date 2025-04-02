da84f6a9027beb34ca6826b3cb48e5ce064a1c07
Gustaf Järgren <gurreja@gmail.com>
2025-04-02 15:29:54 +0200

    chore: linted all files

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,16 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-import App from "./App.vue";
-import router from "./router";
-import i18n from "./i18n";
+import App from './App.vue'
+import router from './router'
+import i18n from './i18n'
 
 import './index.css'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
-app.use(i18n);
+app.use(createPinia())
+app.use(router)
+app.use(i18n)
 
-app.mount("#app");
+app.mount('#app')
